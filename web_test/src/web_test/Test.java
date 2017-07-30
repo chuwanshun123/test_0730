f738825e84b8c1ea7e470164d782a3701b8aade5
chuwanshun <chuwanshun@163.com>
2017-07-30 16:25:50 +0800

    7月30号测试服务器和数据库连接成功

---
+++ web_test/src/web_test/Test.java
@@ -0,0 +1,35 @@
+package web_test;
+
+
+import java.sql.*;
+public class Test {
+  
+     public static void main(String args[]) {  
+     try {  
+          Class.forName("com.mysql.jdbc.Driver");  //加载MYSQL JDBC驱动程序  
+          //Class.forName("org.gjt.mm.mysql.Driver");  
+          System.out.println("Success loading Mysql Driver!");  
+         }catch (Exception e) {  
+          System.out.print("Error loading Mysql Driver!");  
+          e.printStackTrace();  
+     }  
+     try{  
+          Connection connect = DriverManager.getConnection(  
+           "jdbc:mysql://localhost:3306/test","root","123456");  
+           //连接URL为 jdbc:mysql//服务器地址/数据库名 ，后面的2个参数分别是登陆用户名和密码  
+           
+          System.out.println("Success connect Mysql server!");  
+          Statement stmt = connect.createStatement();  
+          ResultSet rs = stmt.executeQuery("select * from tutorials_tbl ");  
+                        //user 为你表的名称  
+          while (rs.next()) {  
+                  System.out.println(rs.getString("tutorial_author"));  
+             }  
+         }catch(Exception e) {  
+          System.out.print("get data error!");  
+          e.printStackTrace();  
+         }  
+    
+     }  
+    
+}
